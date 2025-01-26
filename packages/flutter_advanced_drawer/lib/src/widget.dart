bffb643b1bcddff4d8a98017870c047707b4e4bc
HarryDeKat <96647011+HarryDeKat@users.noreply.github.com>
2025-01-26 13:12:29 +0100

    ↪️ Reversed changed in Advanced drawer

--- packages/flutter_advanced_drawer/lib/src/widget.dart
+++ packages/flutter_advanced_drawer/lib/src/widget.dart
@@ -21,20 +21,50 @@ class AdvancedDrawer extends StatefulWidget {
     this.animationController,
   }) : super(key: key);
 
+  /// Child widget. (Usually widget that represent a screen)
   final Widget child;
+
+  /// Drawer widget. (Widget behind the [child]).
   final Widget drawer;
+
+  /// Controller that controls widget state.
   final AdvancedDrawerController? controller;
+
+  /// Backdrop color.
   final Color? backdropColor;
+
+  /// Backdrop widget for custom background.
   final Widget? backdrop;
+
+  /// Opening ratio.
   final double openRatio;
+
+  /// Opening ratio.
   final double openScale;
+
+  /// Animation duration.
   final Duration animationDuration;
+
+  /// Animation curve.
   final Curve? animationCurve;
+
+  /// Child container decoration in open widget state.
   final BoxDecoration? childDecoration;
+
+  /// Indicates that [childDecoration] might be animated or not.
+  /// NOTICE: It may cause animation jerks.
   final bool animateChildDecoration;
+
+  /// Opening from Right-to-left.
   final bool rtlOpening;
+
+  /// Disable gestures.
   final bool disabledGestures;
+
+  /// Disable fling to open and close the drawer. Only works when [disabledGestures] is false.
   final bool disableFling;
+
+  /// Controller that controls widget animation.
   final AnimationController? animationController;
 
   @override
@@ -45,14 +75,14 @@ class _AdvancedDrawerState extends State<AdvancedDrawer>
     with TickerProviderStateMixin {
   final _spareController = AdvancedDrawerController();
 
-  AnimationController? _spareAnimationController;
-  AnimationController? _animationController;
+  late AnimationController _spareAnimationController;
+  late AnimationController _animationController;
 
-  Animation<double>? _drawerScaleAnimation;
-  Animation<double>? _drawerFadeAnimation;
-  Animation<Offset>? _childSlideAnimation;
-  Animation<double>? _childScaleAnimation;
-  Animation<Decoration>? _childDecorationAnimation;
+  late Animation<double> _drawerScaleAnimation;
+  late Animation<double> _drawerFadeAnimation;
+  late Animation<Offset> _childSlideAnimation;
+  late Animation<double> _childScaleAnimation;
+  late Animation<Decoration> _childDecorationAnimation;
 
   late double _offsetValue;
   late Offset _freshPosition;
@@ -63,19 +93,15 @@ class _AdvancedDrawerState extends State<AdvancedDrawer>
   @override
   void initState() {
     super.initState();
-    _controller.addListener(_handleControllerChanged);
+
+    _initControllers();
   }
 
   @override
   void didUpdateWidget(covariant AdvancedDrawer oldWidget) {
+    _initControllers();
+
     super.didUpdateWidget(oldWidget);
-    if (widget.controller != oldWidget.controller) {
-      oldWidget.controller?.removeListener(_handleControllerChanged);
-      _controller.addListener(_handleControllerChanged);
-    }
-    if (widget.animationController != oldWidget.animationController) {
-      _animationController = null; // Reset to recreate on-demand
-    }
   }
 
   @override
@@ -100,12 +126,12 @@ class _AdvancedDrawerState extends State<AdvancedDrawer>
               child: FractionallySizedBox(
                 widthFactor: widget.openRatio,
                 child: ScaleTransition(
-                  scale: _drawerScaleAnimation!,
+                  scale: _drawerScaleAnimation,
                   alignment: widget.rtlOpening
                       ? Alignment.centerLeft
                       : Alignment.centerRight,
                   child: FadeTransition(
-                    opacity: _drawerFadeAnimation!,
+                    opacity: _drawerFadeAnimation,
                     child: RepaintBoundary(
                       child: widget.drawer,
                     ),
@@ -114,38 +140,55 @@ class _AdvancedDrawerState extends State<AdvancedDrawer>
               ),
             ),
             SlideTransition(
-              position: _childSlideAnimation!,
+              position: _childSlideAnimation,
               textDirection:
                   widget.rtlOpening ? TextDirection.rtl : TextDirection.ltr,
               child: ScaleTransition(
-                scale: _childScaleAnimation!,
+                scale: _childScaleAnimation,
                 alignment: widget.rtlOpening
                     ? Alignment.centerRight
                     : Alignment.centerLeft,
-                child: Container(
-                  clipBehavior: widget.childDecoration != null
-                      ? Clip.antiAlias
-                      : Clip.none,
-                  decoration: widget.animateChildDecoration &&
-                          widget.childDecoration != null
-                      ? _childDecorationAnimation?.value
-                      : widget.childDecoration,
-                  child: RepaintBoundary(
-                    child: ValueListenableBuilder<AdvancedDrawerValue>(
+                child: Builder(
+                  builder: (_) {
+                    final childStack =
+                        ValueListenableBuilder<AdvancedDrawerValue>(
                       valueListenable: _controller,
                       builder: (_, value, __) {
                         return InkWell(
-                          onTap: value.visible ? _controller.hideDrawer : null,
+                          onTap: _controller.hideDrawer,
                           overlayColor: const MaterialStatePropertyAll(
                               Colors.transparent),
                           child: AbsorbPointer(
                             absorbing: value.visible,
-                            child: widget.child,
+                            child: RepaintBoundary(child: widget.child),
                           ),
                         );
                       },
-                    ),
-                  ),
+                    );
+
+                    if (widget.animateChildDecoration &&
+                        widget.childDecoration != null) {
+                      return AnimatedBuilder(
+                        animation: _childDecorationAnimation,
+                        builder: (_, child) {
+                          return Container(
+                            clipBehavior: Clip.antiAlias,
+                            decoration: _childDecorationAnimation.value,
+                            child: child,
+                          );
+                        },
+                        child: childStack,
+                      );
+                    }
+
+                    return Container(
+                      clipBehavior: widget.childDecoration != null
+                          ? Clip.antiAlias
+                          : Clip.none,
+                      decoration: widget.childDecoration,
+                      child: childStack,
+                    );
+                  },
                 ),
               ),
             ),
@@ -159,33 +202,27 @@ class _AdvancedDrawerState extends State<AdvancedDrawer>
     return widget.controller ?? _spareController;
   }
 
-  AnimationController get _getSpareAnimationController {
-    if (_spareAnimationController == null) {
-      _spareAnimationController = AnimationController(
-        vsync: this,
-        value: _controller.value.visible ? 1 : 0,
-      );
-    }
-    return _spareAnimationController!;
-  }
+  void _initControllers() {
+    _controller
+      ..removeListener(_handleControllerChanged)
+      ..addListener(_handleControllerChanged);
 
-  AnimationController get _getAnimationController {
-    if (_animationController == null) {
-      _animationController =
-          widget.animationController ?? _getSpareAnimationController;
-      _animationController!.reverseDuration =
-          _animationController!.duration = widget.animationDuration;
-      _initAnimations();
-    }
-    return _animationController!;
-  }
+    _spareAnimationController = AnimationController(
+      vsync: this,
+      value: _controller.value.visible ? 1 : 0,
+    );
 
-  void _initAnimations() {
+    _animationController =
+        widget.animationController ?? _spareAnimationController;
+
+    _animationController.reverseDuration =
+        _animationController.duration = widget.animationDuration;
+
     final parentAnimation = widget.animationCurve == null
-        ? _getAnimationController
+        ? _animationController
         : CurvedAnimation(
             curve: widget.animationCurve!,
-            parent: _getAnimationController,
+            parent: _animationController,
           );
 
     _drawerScaleAnimation = Tween<double>(
@@ -208,36 +245,38 @@ class _AdvancedDrawerState extends State<AdvancedDrawer>
       end: widget.openScale,
     ).animate(parentAnimation);
 
-    if (widget.animateChildDecoration && widget.childDecoration != null) {
-      _childDecorationAnimation = DecorationTween(
-        begin: const BoxDecoration(),
-        end: widget.childDecoration,
-      ).animate(parentAnimation);
-    }
+    _childDecorationAnimation = DecorationTween(
+      begin: const BoxDecoration(),
+      end: widget.childDecoration,
+    ).animate(parentAnimation);
   }
 
   void _handleControllerChanged() {
+    // Check if the widget is still mounted
     if (context.mounted) {
+      // If the value of _controller is visible, forward the animation; otherwise, reverse it
       _controller.value.visible
-          ? _getAnimationController.forward()
-          : _getAnimationController.reverse();
+          ? _animationController.forward()
+          : _animationController.reverse();
     }
   }
 
   void _handleDragStart(DragStartDetails details) {
     _captured = true;
     _startPosition = details.globalPosition;
-    _offsetValue = _getAnimationController.value;
+    _offsetValue = _animationController.value;
   }
 
   void _handleDragUpdate(DragUpdateDetails details) {
     if (!_captured) return;
 
-    final screenSize = MediaQuery.of(context).size; // Cache MediaQuery
+    final screenSize = MediaQuery.of(context).size;
+
     _freshPosition = details.globalPosition;
 
     final diff = (_freshPosition - _startPosition!).dx;
-    _getAnimationController.value = _offsetValue +
+
+    _animationController.value = _offsetValue +
         (diff / (screenSize.width * widget.openRatio)) *
             (widget.rtlOpening ? -1 : 1);
   }
@@ -248,15 +287,27 @@ class _AdvancedDrawerState extends State<AdvancedDrawer>
     _captured = false;
 
     double velocity = details.primaryVelocity! * (widget.rtlOpening ? -1 : 1);
-    bool isOpening = _getAnimationController.value >= 0.5;
-    if (velocity.abs() >= kMinFlingVelocity && !widget.disableFling) {
-      isOpening = velocity > 0;
-    }
+    bool isOpening = false;
+
+    isOpening = (_animationController.value >= 0.5);
+    isOpening = (velocity >= kMinFlingVelocity && !widget.disableFling)
+        ? true
+        : (velocity <= kMinFlingVelocity * -1 && !widget.disableFling)
+            ? false
+            : isOpening;
 
     if (isOpening) {
-      if (mounted) _controller.showDrawer();
+      if (_controller.value.visible) {
+        _animationController.forward();
+      } else {
+        if (mounted) _controller.showDrawer();
+      }
     } else {
-      if (mounted) _controller.hideDrawer();
+      if (!_controller.value.visible) {
+        _animationController.reverse();
+      } else {
+        if (mounted) _controller.hideDrawer();
+      }
     }
   }
 
@@ -266,8 +317,12 @@ class _AdvancedDrawerState extends State<AdvancedDrawer>
 
   @override
   void dispose() {
-    _controller.removeListener(_handleControllerChanged);
-    _spareAnimationController?.dispose();
+    _spareController
+      ..removeListener(_handleControllerChanged)
+      ..dispose();
+
+    _spareAnimationController.dispose();
+
     super.dispose();
   }
 }
